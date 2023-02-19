e2974ea6543ff338543f175869ccbc8922819669
Miguelhyto <57653360+miguelhito54@users.noreply.github.com>
2023-02-18 22:07:57 -0500

    Extra learning

---
+++ The_legend_of_python/initials.py
@@ -0,0 +1,12 @@
+# Fun fact is that i still love my ex 
+
+# Miguel                          #Angel
+print('')
+print('M','     ',      'M',"     ",'   ','A')
+print('M','M',' ','M','M',"     ",' ','A',' ','A')
+print('M','M','M','M','M',"     ",'A','     ','A')
+print('M'  ,'  M  ',   'M',"     ",'A','A','A','A','A')
+print('M','     ',     'M',"     ",'A','     ','A')
+print('M','     ',     'M',"     ",'A','     ','A')
+
+
---
+++ The_legend_of_python/letter.py
@@ -0,0 +1,10 @@
+print("6 feb 2023")
+print('i feel sad, because of the differente circumstances and the different things that ive'
+' and i been experimenting lately. but here im, learning skills to get ahead with my future, i know ill be succesful'
+' i believe in myself <3 and if youre reading'
+' this when youre that man. please remember the 20 year old you that pain that you went through, you deserve'
+' evrityng you have')
+print('i want to have fun while practicing a little bit python')
+print('hey Miguel you are gonna find that woman that loves you, dont worry if you found her let her see this so shes gonna know'
+' that she is the one ')
+print('🍑')
